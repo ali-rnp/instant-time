1e77bca506d77325559ffa5015c1c726f4501ad8
ali-rnp <a.ravannakhjavani@gmail.com>
2023-08-15 22:14:25 +0330

    implement getMonthValue() method

--- src/main/java/el/preacher/InstantTime.java
+++ src/main/java/el/preacher/InstantTime.java
@@ -3,6 +3,7 @@ package el.preacher;
 import java.time.Instant;
 
 public class InstantTime {
+
     public int getYear(Instant instant){
         if (instant == null)
             return -1;
@@ -11,4 +12,14 @@ public class InstantTime {
                         .substring(0, 4)
         );
     }
+
+    public int getMonthValue(Instant instant){
+        if (instant == null)
+            return -1;
+        return Integer.parseInt(
+                instant.toString()
+                        .substring(5, 7)
+        );
+    }
+
 }
