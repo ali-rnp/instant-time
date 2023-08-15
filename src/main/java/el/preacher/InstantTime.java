e14ea19e21cbaa29c606e47ab995790ef2cb3e05
ali-rnp <a.ravannakhjavani@gmail.com>
2023-08-15 21:57:24 +0330

    implement getYear() method

--- src/main/java/el/preacher/InstantTime.java
+++ src/main/java/el/preacher/InstantTime.java
@@ -1,4 +1,14 @@
 package el.preacher;
 
+import java.time.Instant;
+
 public class InstantTime {
+    public int getYear(Instant instant){
+        if (instant == null)
+            return -1;
+        return Integer.parseInt(
+                instant.toString()
+                        .substring(0, 4)
+        );
+    }
 }
